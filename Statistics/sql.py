f0676c7c48da1e6b09604e4100b2c17d0a463815
Gaetan Morand <74169342+morand-g@users.noreply.github.com>
2020-11-27 16:01:48 +0600

    Add files via upload

---
+++ Statistics/sql.py
@@ -0,0 +1,154 @@
+import MySQLdb
+from random import sample
+from datetime import datetime, date, timedelta
+import pandas as pd
+
+
+def OpenMydb(database="Reefscapers2020"):
+
+    # Opens the specified database
+
+    db = MySQLdb.connect(host="", user="", passwd="", db=database)
+    db.autocommit(True)
+    return db
+
+
+def download_frag_data():
+
+    request = """SELECT Frags.FragmentId, FSFrames.Transplanted AS StartDate, Frags.Transplanted,
+                    T, E, Frags.Type, Size, Zone, Depth, Elev, LEFT(Sizes.Tag, 2) as Resort FROM 
+            (
+                    SELECT Fragments.FragmentId, Transplanted, DATEDIFF(STR_TO_DATE(MAX(Date),'%y%m%d'), Transplanted) AS T, 0 AS E, Fragments.Type, RIGHT(Bar,1) AS Elev FROM `Fragments`
+                    INNER JOIN Status ON Fragments.FragmentId = Status.FragmentId
+                    WHERE Status.Type IN ('Live Coral', 'Bleached') AND Dead IS NULL AND Fragments.Type IN ('Pocillopora', 'Acropora') 
+                    GROUP BY FragmentId
+                UNION SELECT FragmentId, Transplanted, DATEDIFF(Dead, Transplanted) AS T, 1 AS E, Type, RIGHT(Bar,1) AS Elev FROM `Fragments` WHERE Type IN ('Pocillopora', 'Acropora') AND Dead IS NOT NULL AND Transplanted IS NOT NULL
+            ) Frags INNER JOIN
+            (
+                
+                SELECT B.FragmentId, ROUND(AdjWidth + AdjHeight, 2) AS Size, Tag
+                FROM
+                (
+                    SELECT FragmentId, MIN(Date) AS min_date
+                    FROM Observations
+                    GROUP BY FragmentId
+                ) B INNER JOIN Observations ON B.FragmentId = Observations.FragmentId AND B.min_date = Observations.Date
+
+            ) Sizes ON Frags.FragmentId = Sizes.FragmentId
+            LEFT JOIN FSFrames ON Sizes.Tag = FSFrames.Tag"""
+
+    db = OpenMydb()
+    cur = db.cursor()
+    cur.execute(request)
+    
+    df = pd.DataFrame(cur.fetchall())
+    desc = cur.description
+    df.columns = [col[0] for col in desc]
+    df['Depth'] = pd.to_numeric(df['Depth'], errors='coerce')
+    df['StartDate'] = df['StartDate'].apply(lambda x:datetime.strptime('20' + str(x).zfill(6), '%Y%m%d'))
+    df['Transplanted'] = pd.to_datetime(df['Transplanted'], format='%Y-%m-%d')
+    df['Sandy'] = df['Zone'].isin(['Blu (LG)', 'Blu Deep (LG)', 'House Reef (KH)'])
+    df.loc[df['Resort'] == 'RE', 'Resort'] = 'LG'
+    
+    db.close()
+    
+    return(df)
+
+
+def get_volume_observations():
+
+    db = OpenMydb()
+    cur = db.cursor()
+
+    request = """SELECT Observations.FragmentId, Date As MonDate,  POWER(AdjHeight * AdjWidth, 1.5) AS vol FROM Observations
+                INNER JOIN Fragments ON Observations.FragmentId = Fragments.FragmentId
+                WHERE Fragments.Type IN ('Pocillopora', 'Acropora') ORDER BY FragmentId, MonDate"""
+    cur.execute(request)
+    df = pd.DataFrame(cur.fetchall())
+    desc = cur.description
+    df.columns = [col[0] for col in desc]
+    df['Date'] = df['MonDate'].apply(lambda x:datetime.strptime('20' + str(x).zfill(6), '%Y%m%d')).dt.date
+
+    request = """SELECT Fragments.FragmentId, Type, Transplanted, Dead, FallDate FROM Fragments 
+                LEFT JOIN (SELECT FragmentId, MIN(Date) AS FallDate FROM Status WHERE Type = 'Fallen' GROUP BY FragmentId ) A
+                ON Fragments.FragmentId = A.FragmentId
+                WHERE Type IN ('Acropora', 'Pocillopora')"""
+    cur.execute(request)
+    frags = {}
+    for row in cur.fetchall():
+        frags[row[0]] = {'Type' : row[1], 'Transplanted' : row[2], 'Dead' : row[3], 'FallDate' : row[4]}
+
+    return(df, frags)
+
+
+def get_mortality():
+
+    db = OpenMydb()
+    cur = db.cursor()
+
+    request = """SELECT Fragments.FragmentId, Fragments.Type AS Specie, Fragments.Tag, Fragments.Transplanted, Zone,
+                Date1, Status1.Type AS Stat1, Date2, Status2.Type AS Stat2
+                FROM Fragments
+                INNER JOIN
+                (   SELECT Status1.FragmentId, Status1.Date As Date1, MIN(Status2.Date) AS Date2
+                    FROM Status Status1
+                    INNER JOIN Status Status2 ON Status1.FragmentId = Status2.FragmentId AND Status1.Date < Status2.Date
+                    GROUP BY FragmentId, Date1) A ON Fragments.FragmentId = A.FragmentId
+                INNER JOIN Status Status1 ON A.FragmentId = Status1.FragmentId AND A.Date1 = Status1.Date
+                INNER JOIN Status Status2 ON A.FragmentId = Status2.FragmentId AND A.Date2 = Status2.Date
+                INNER JOIN FSFrames ON Fragments.Tag = FSFrames.Tag
+                WHERE Fragments.Type IN ('Pocillopora', 'Acropora')"""
+    cur.execute(request)
+    data = pd.DataFrame(cur.fetchall())
+    desc = cur.description
+    data.columns = [col[0] for col in desc]
+
+    data['D1'] = data['Date1'].apply(lambda x:datetime.strptime('20' + str(x).zfill(6), '%Y%m%d'))
+    data['D2'] = data['Date2'].apply(lambda x:datetime.strptime('20' + str(x).zfill(6), '%Y%m%d'))
+    data['Median'] = data['D1'] + (data['D2'] - data['D1']) / 2
+    data['tdelta'] = data['Median'].dt.date - data['Transplanted']
+    data = data[data['tdelta'].dt.days > 180]
+
+    survived = data[(data['Stat1'] == 'Live Coral') & (data['Stat2'] == 'Live Coral')]
+    dead = data[(data['Stat1'] == 'Live Coral') & (data['Stat2'] != 'Live Coral')]
+
+    return(survived, dead)
+
+
+def get_growth_data():
+
+    mainzones = ['Al Barakat (LG)', 'Anchor Point (LG)', 'Blu (LG)', 'Blu Deep (LG)', 'Coral Trail (LG)', 'Dive Site (LG)',
+              'Water Villas (LG)', 'Blue Holes (LG)', 'Parrot Reef (LG)', 'Water Villas (KH)', 'House Reef (KH)', 'Channel (KH)']
+    db = OpenMydb()
+    cur = db.cursor()
+
+    request = """SELECT Obs1.FragmentId, Fragments.Type, Fragments.Tag, Zone,
+                Obs1.Date As Date1, Status1.Type AS Stat1, 0.5*(Obs1.AdjHeight + Obs1.AdjWidth) AS len1,
+                Obs2.Date As Date2, Status2.Type AS Stat2, 0.5*(Obs2.AdjHeight + Obs2.AdjWidth) AS len2
+                FROM Observations Obs1
+                INNER JOIN Observations Obs2 ON Obs1.FragmentId = Obs2.FragmentId AND Obs1.Date < Obs2.Date
+                INNER JOIN Fragments ON Obs1.FragmentId = Fragments.FragmentId
+                INNER JOIN FSFrames ON Fragments.Tag = FSFrames.Tag
+                INNER JOIN Status Status1 ON Obs1.FragmentId = Status1.FragmentId AND Obs1.Date = Status1.Date
+                INNER JOIN Status Status2 ON Obs2.FragmentId = Status2.FragmentId AND Obs2.Date = Status2.Date
+                WHERE Fragments.Type IN ('Pocillopora', 'Acropora')"""
+    cur.execute(request)
+    data = pd.DataFrame(cur.fetchall())
+    desc = cur.description
+    data.columns = [col[0] for col in desc]
+
+    df = data.copy()
+    df['len1'], df['len2'] = 0.3296 * df['len1'], 0.3296 * df['len2']
+    df['NDate1'] = df['Date1'].apply(lambda x:datetime.strptime('20' + str(x).zfill(6), '%Y%m%d'))
+    df['NDate2'] = df['Date2'].apply(lambda x:datetime.strptime('20' + str(x).zfill(6), '%Y%m%d'))
+    df['Median'] = (df['NDate1'] + (df['NDate2'] - df['NDate1']) / 2).apply(lambda x: str(x.year) + 'M' + str(x.month).zfill(2))
+    df['tdelta'] = (df['NDate2'] - df['NDate1']).dt.days / 365
+    df['avgrowth'] = (df['len2'] - df['len1'])/df['tdelta']
+    df = df[df['tdelta'] > 5/12]
+    df = df[df['Zone'].isin(mainzones)]
+    livedf = df[(df['Stat1'] == 'Live Coral') & (df['Stat2'] == 'Live Coral')]
+    livedf = livedf.groupby(['Tag', 'FragmentId', 'Type', 'Zone']).mean()
+    livedf.reset_index(level=[0,1,2,3], inplace=True)
+    livedf.set_index('FragmentId', inplace = True)
+
+    return(livedf)
---
+++ Statistics/stats.py
@@ -0,0 +1,162 @@
+import pandas as pd
+import numpy as np
+
+from sql import *
+
+
+
+def split_data(fragdata):
+
+    dic = {}
+
+    dic['all'] = fragdata
+
+    poc = fragdata[fragdata['Type'] == 'Pocillopora']
+    acro = fragdata[fragdata['Type'] == 'Acropora']
+
+    dic['smallpoc'] = poc[poc['Size'] <= poc['Size'].median()]
+    dic['largepoc'] = poc[poc['Size'] > poc['Size'].median()]
+    dic['smallacro'] = acro[acro['Size'] <= acro['Size'].median()]
+    dic['largeacro'] = acro[acro['Size'] > acro['Size'].median()]
+
+    lg = fragdata[fragdata['Resort'] == 'LG']
+    dic['lgpoc'] = lg[lg['Type'] == 'Pocillopora']
+    dic['lgacro'] = lg[lg['Type'] == 'Acropora']
+    kh = fragdata[fragdata['Resort'] == 'KH']
+    dic['khpoc'] = kh[kh['Type'] == 'Pocillopora']
+    dic['khacro'] = kh[kh['Type'] == 'Acropora']
+
+    zones = ['KH Water Villas', 'LG Water Villas', 'Channel', 'Parrot Reef', 'Blu', 'Blu Deep', 'House Reef', 'Blue Holes', 'Dive Site', 'Coral Trail', 'Anchor Point', 'Al Barakat']
+    for z in zones:
+        dic[z] = fragdata[fragdata['Zone'] == z]
+
+    shallow = fragdata[fragdata['Depth'] <= 3]
+    dic['shallow'] = shallow
+    dic['medium'] = fragdata[(fragdata['Depth'] > 3) & (fragdata['Depth'] <= 7)]
+    dic['deep'] = fragdata[fragdata['Depth'] > 7]
+
+    dic['shaded'] = shallow[shallow['Zone'] == 'KH Water Villas']
+    dic['non-shaded'] = shallow[~shallow['Zone'].isin(['KH Water Villas', 'LG Water Villas'])]
+
+    dic['a'] = fragdata[fragdata['Elev'] == 'A']
+    dic['b'] = fragdata[fragdata['Elev'] == 'B']
+    dic['c'] = fragdata[fragdata['Elev'] == 'C']
+
+    dic['lg'], dic['kh'], dic['poc'], dic['acro'] = lg, kh, poc, acro
+
+    return(dic)
+
+
+def get_volume_series():
+
+    df, frags = get_volume_observations()
+    dates = list(pd.date_range(date(2016, 1, 1), date.today(), freq='d'))
+    fallen = pd.DataFrame(0, index=dates, columns=['Acropora', 'Pocillopora'])
+    dead = pd.DataFrame(0, index=dates, columns=['Acropora', 'Pocillopora'])
+    live = pd.DataFrame(0, index=dates, columns=['Acropora', 'Pocillopora'])
+    initial = pd.DataFrame(0, index=dates, columns=['Acropora', 'Pocillopora'])
+
+    for frag_id in pd.unique(df['FragmentId']):
+        t = frags[frag_id]['Type']
+        obs = df[df['FragmentId'] == frag_id].reset_index().to_dict('index')
+        obs_count = len(obs.keys())
+        date0 = frags[frag_id]['Transplanted']
+        init_volume = obs[0]['vol']
+        firstdead = obs_count
+
+        if frags[frag_id]['Dead'] is None:
+
+            for i in range(1, obs_count):
+                date1 = obs[i]['Date']
+                added = pd.Series(obs[i - 1]['vol'], index=list(pd.date_range(date0, date1, freq='d', closed='left')))
+                live[t] = live[t].add(added, fill_value = 0)
+                date0 = date1
+
+            init_added = pd.Series(init_volume, index=list(pd.date_range(frags[frag_id]['Transplanted'], date.today(), freq='d')))
+            initial[t] = initial[t].add(init_added, fill_value = 0)
+            added = pd.Series(obs[obs_count - 1]['vol'], index=list(pd.date_range(obs[obs_count - 1]['Date'], date.today(), freq='d')))
+            live[t] = live[t].add(added, fill_value = 0)
+
+
+        else:
+            i = 1
+            while i < obs_count and obs[i]['Date'] < frags[frag_id]['Dead']:
+                date1 = obs[i]['Date']
+                added = pd.Series(obs[i - 1]['vol'], index=list(pd.date_range(date0, date1, freq='d', closed='left')))
+                live[t] = live[t].add(added, fill_value = 0)
+                date0 = date1
+                i += 1
+            added = pd.Series(obs[i - 1]['vol'], index=list(pd.date_range(date0, frags[frag_id]['Dead'], freq='d', closed='left')))
+            live[t] = live[t].add(added, fill_value = 0)
+            init_added = pd.Series(init_volume, index=list(pd.date_range(frags[frag_id]['Transplanted'], frags[frag_id]['Dead'], freq='d', closed='left')))
+            initial[t] = initial[t].add(init_added, fill_value = 0)
+
+            if i < obs_count:
+                added = pd.Series(obs[i - 1]['vol'], index=list(pd.date_range(frags[frag_id]['Dead'], obs[i]['Date'], freq='d', closed='left')))
+                dead[t] = dead[t].add(added, fill_value = 0)
+                date0 = obs[i]['Date']
+                for j in range(i, obs_count - 2):
+                    date1 = obs[j + 1]['Date']
+                    added = pd.Series(obs[j]['vol'], index=list(pd.date_range(date0, date1, freq='d', closed='left')))
+                    dead[t] = dead[t].add(added, fill_value = 0)
+                    date0 = date1
+
+            added = pd.Series(obs[obs_count - 1]['vol'], index=list(pd.date_range(obs[obs_count - 1]['Date'], date.today(), freq='d')))
+            dead[t] = dead[t].add(added, fill_value = 0)
+
+            if frags[frag_id]['FallDate'] is not None:
+                act_date = obs[obs_count - 1]['Date'] + (datetime.strptime('20' + str(frags[frag_id]['FallDate']).zfill(6), '%Y%m%d').date() - obs[obs_count - 1]['Date'])/ 2
+                added = pd.Series(obs[obs_count - 1]['vol'], index=list(pd.date_range(act_date, date.today(), freq='d')))
+                fallen[t] = fallen[t].add(added, fill_value = 0)
+
+
+    return(0.1568*0.001*live, 0.1568*0.001*dead, 0.1568*0.001*initial, 0.1568*0.001*fallen)
+
+
+def get_disrupted_frag_data(disruption = ''):
+
+    fragdata = split_data(download_frag_data())
+    disruptedzones = ['Water Villas (LG)', 'Channel (KH)', 'Blue Holes (LG)', 'Parrot Reef (LG)', 'Water Villas (KH)', 'House Reef (KH)']
+    df = fragdata['all'][fragdata['all']['Zone'].isin(disruptedzones)]
+
+    disruptions = {'Water Villas (LG)': ['2019-06-01', '2020-03-01'], 'Channel (KH)': ['2018-05-01', '2020-02-01'],
+                 'Parrot Reef (LG)': ['2017-02-15', '2018-08-01'], 'Blue Holes (LG)': ['2019-06-01', '2020-03-01'],
+                 'Water Villas (KH)': ['2019-03-30', '2019-09-30'], 'House Reef (KH)': ['2019-03-30', '2019-09-30']}
+
+    df['Size'] = 0.3296*df['Size']
+    df['tdelta'] = (df['Transplanted'] - df['StartDate']).dt.days <= 90
+    df = df[df['tdelta']]
+    df['Start'], df['Stop'], df['Disrupted'] = 0, 0, 0
+    df['Transplanted'] = df['Transplanted'].dt.date
+    df['Deep'] = 1*(df['Depth'] > 3)
+
+    long_df = pd.DataFrame(columns = list(df.columns))
+
+    n0 = 4 if disruption == 'heatwave' else 0
+
+    for z in disruptedzones[n0:]:
+        zdf = df.loc[df['Zone'] == z].copy()
+        s, e = datetime.strptime(disruptions[z][0], '%Y-%m-%d').date(), datetime.strptime(disruptions[z][1], '%Y-%m-%d').date()
+        
+        beginning = zdf.loc[zdf['Transplanted'] < s].copy()
+        beginning['Stop'] = pd.concat([beginning['T'], (s-beginning['Transplanted']).dt.days], axis=1).min(axis=1)
+        beginning['E'] = zdf['E']*(beginning['Stop'] == beginning['T'])
+        long_df = long_df.append(beginning, ignore_index = True)
+        
+        middle = zdf.loc[(zdf['Transplanted'] < e) & (zdf['Transplanted'] + pd.to_timedelta(zdf['T'], 'D') > s)].copy()
+        middle['Start'] = (s - middle['Transplanted']).dt.days.clip(lower = 0)
+        middle['Disrupted'] = 1
+        middle['Stop'] = pd.concat([(e - middle['Transplanted']).dt.days, zdf['T']], axis=1).min(axis=1)
+        middle['E'] = zdf['E']*(middle['Stop'] == middle['T'])
+        long_df = long_df.append(middle, ignore_index = True)
+        
+        ending = zdf.loc[zdf['Transplanted'] + pd.to_timedelta(zdf['T'], 'D') > e].copy()
+        ending['Start'] = (e - ending['Transplanted']).dt.days.clip(lower = 0)
+        ending['Stop'] = ending['T']
+        ending['E'] = zdf['E']*(ending['Stop'] == ending['T'])
+        long_df = long_df.append(ending, ignore_index = True)
+
+    long_df = long_df.loc[~((long_df['Start'] == long_df['Stop']) & (long_df['Start'] == 0))]
+    long_df = long_df.sort_values(['FragmentId', 'Start'])
+
+    return(long_df)
